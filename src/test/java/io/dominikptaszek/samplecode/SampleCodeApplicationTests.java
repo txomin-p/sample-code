e2c17e583f8781236b45e6184caab48c699fde0a
Txomin <dominik.ptaszek@proton.me>
2023-09-03 16:12:24 +0200

    initial commit

---
+++ src/test/java/io/dominikptaszek/samplecode/SampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dominikptaszek.samplecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
